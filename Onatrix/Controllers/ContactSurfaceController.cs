657ade12961a1b45a0b9f5a272e2172d8539a17c
Nike <nikesjolin@outlook.com>
2024-09-17 14:57:55 +0200

    added more to page and surfacecontroller and model

---
+++ Onatrix/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Onatrix.Models;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace Onatrix.Controllers
+{
+	public class ContactSurfaceController : SurfaceController
+	{
+		public ContactSurfaceController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+		{
+		}
+
+		public IActionResult HandleSubmit(ContactFormModel form)
+		{
+			if (!ModelState.IsValid) 
+			{
+				ViewData["name"] = form.Name;
+				ViewData["email"] = form.Email;
+				ViewData["phone"] = form.Phone;
+
+				ViewData["error_name"] = string.IsNullOrEmpty(form.Name);
+				ViewData["error_email"] = string.IsNullOrEmpty(form.Email);
+				ViewData["error_phone"] = string.IsNullOrEmpty(form.Phone);
+
+				return CurrentUmbracoPage();
+			}
+
+			TempData["success"] = "Form submitted successfully.";
+			return RedirectToCurrentUmbracoPage();
+		}
+	}
+}
