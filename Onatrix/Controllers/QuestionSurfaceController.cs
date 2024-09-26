163222275ed1a342e09dbb1a4f51285305f3ea3b
Nike <nikesjolin@outlook.com>
2024-09-26 14:55:45 +0200

    Added question form

---
+++ Onatrix/Controllers/QuestionSurfaceController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Onatrix.Models;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace Onatrix.Controllers
+{
+	public class QuestionSurfaceController : SurfaceController
+	{
+		public QuestionSurfaceController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+		{
+		}
+
+		public IActionResult QuestionSubmit(QuestionFormModel form)
+		{
+			if (!ModelState.IsValid)
+			{
+				ViewData["question_name"] = form.Name;
+				ViewData["question_email"] = form.Email;
+				ViewData["question_message"] = form.Message;
+
+				ViewData["question_error_name"] = string.IsNullOrEmpty(form.Name);
+				ViewData["question_error_email"] = string.IsNullOrEmpty(form.Email);
+				ViewData["question_error_message"] = string.IsNullOrEmpty(form.Message);
+
+				return CurrentUmbracoPage();
+			}
+
+			TempData["question_success"] = "form submitted sucessfully";
+			return RedirectToCurrentUmbracoPage();
+		}
+	}
+}
---
+++ Onatrix/Models/QuestionFormModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Onatrix.Models;
+
+public class QuestionFormModel
+{
+	public string Name { get; set; } = null!;
+	public string Email { get; set; } = null!;
+	public string Message { get; set; } = null!;
+}
